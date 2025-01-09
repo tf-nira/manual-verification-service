19e9f42294c8972958ec7a87855008dc56a714e8
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-01-09 17:29:37 +0530

    removed java version log

--- src/main/java/in/tf/nira/manual/verification/ManualVerificationApplication.java
+++ src/main/java/in/tf/nira/manual/verification/ManualVerificationApplication.java
@@ -1,10 +1,5 @@
 package in.tf.nira.manual.verification;
 
-import java.security.Security;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -19,27 +14,11 @@ import in.tf.nira.manual.verification.listener.Listener;
 @EnableJpaRepositories(basePackages = "in.tf.nira.manual.verification.repository")
 @EnableScheduling
 public class ManualVerificationApplication {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ManualVerificationApplication.class);
 
 	public static void main(String[] args) {
-		logJavaVersion();
-		logBouncyCastleVersion();
 		ConfigurableApplicationContext configurableApplcnConetxt = SpringApplication.run(ManualVerificationApplication.class, args);
         Listener listener = configurableApplcnConetxt.getBean(Listener.class);
         listener.runVerificationQueue();
-	}
-	
-	public static void logJavaVersion() {
-		String javaVersion = System.getProperty("java.version");
-		logger.info("Java Version: {}", javaVersion);
-	}
-
-	public static void logBouncyCastleVersion() {
-		Security.addProvider(new BouncyCastleProvider());
-		BouncyCastleProvider provider = new BouncyCastleProvider(); 
-		Double bcVersion = provider.getVersion();
-		logger.info("Bouncy Castle Version: {}", bcVersion);
 	}
 
 }
