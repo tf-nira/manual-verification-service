00d6c85dec1812b39d55bae6321cb0bc866afbea
Soumya Raj <soumya.raj@technoforte.co.in>
2025-01-10 15:02:33 +0530

    added cookie in auth response header

--- src/main/java/in/tf/nira/manual/verification/controller/AuthController.java
+++ src/main/java/in/tf/nira/manual/verification/controller/AuthController.java
@@ -31,9 +31,7 @@ public class AuthController {
         AuthenticationResponse authenticationResponse = authService.loginClient(authRequest);
 
         String token = authenticationResponse.getToken();
-        String cookieValue = String.format("Authorization=%s; SameSite=None; Path=/; Max-Age=%d",
-                token,
-                authenticationResponse.getExpiryTime());
+        String cookieValue = String.format("Authorization=%s", token);
 
         httpServletResponse.addHeader("Set-Cookie", cookieValue);
 
