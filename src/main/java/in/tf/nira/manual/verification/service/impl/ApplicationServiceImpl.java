92c48262766a8511a6ea9e991114c89aa21b48c5
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2025-01-30 14:57:45 +0530

    remove approve functionality from upload doc(bug fix NPU-18)
    remove approve functionality from upload doc(bug fix NPU-18)

--- src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
+++ src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
@@ -386,7 +386,6 @@ public class ApplicationServiceImpl implements ApplicationService {
 		
 		if(application.getAssignedOfficerRole().equals(CommonConstants.MVS_DISTRICT_OFFICER_ROLE)) {
 			uploadToPacketManager(application, documentDTO);
-			approveApplication(application, "Documents uploaded");
 		}
 		else {
 			logger.error("{} not allowed to upload documents", application.getAssignedOfficerRole());
