2c423df9366575b626a604d50ee76b7fd8a68cf9
Soumya Raj <soumya.raj@technoforte.co.in>
2025-03-10 16:53:46 +0530

    added api to fetch rejected application

---
+++ src/main/java/in/tf/nira/manual/verification/dto/RejectedApplicationResponse.java
@@ -0,0 +1,22 @@
+package in.tf.nira.manual.verification.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RejectedApplicationResponse {
+    private String applicationId;
+    private String service;
+    private String serviceType;
+    private String status;
+    private LocalDateTime crDTimes;
+    private EscalationDetailsDTO officerEscDetails;
+    private EscalationDetailsDTO supervisorEscDetails;
+    private EscalationDetailsDTO legalEscDetails;
+    private String rejectionCategory;
+    private String rejectionComment;
+    private String statusComment;
+    private String foundLink;
+    private String ageGroup;
+}
