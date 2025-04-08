d309a3b940c2422fe731ff7b8901a88686779f5a
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2025-04-08 17:23:38 +0530

    fix fetch only rejected application based on application id

--- src/main/java/in/tf/nira/manual/verification/repository/MVSApplicationRepo.java
+++ src/main/java/in/tf/nira/manual/verification/repository/MVSApplicationRepo.java
@@ -19,7 +19,7 @@ public interface MVSApplicationRepo extends JpaRepository<MVSApplication, String
 	@Query(value = "SELECT * FROM mvs_application a WHERE a.stage = 'REJECTED'", nativeQuery = true)
 	List<MVSApplication> getAllRejectedApplications();
 
-	@Query(value = "SELECT * FROM mvs_application a WHERE a.reg_id = :applicationId", nativeQuery = true)
+	@Query(value = "SELECT * FROM mvs_application a WHERE a.reg_id = :applicationId AND a.stage = 'REJECTED'", nativeQuery = true)
 	MVSApplication getRejectedApplicationById(@Param("applicationId") String applicationId);
 
 	@Query("SELECT e FROM mvs_application e " +
