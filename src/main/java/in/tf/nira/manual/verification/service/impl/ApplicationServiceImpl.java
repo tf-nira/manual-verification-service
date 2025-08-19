2bacdec0b1c017bb6178359c2442a8b14d31b692
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2025-08-19 14:30:31 +0530

    Added service from mvs response to mvs stage

--- src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
+++ src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
@@ -51,6 +51,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import in.tf.nira.manual.verification.config.ServiceProperties;
 import in.tf.nira.manual.verification.constant.CommonConstants;
 import in.tf.nira.manual.verification.constant.ErrorCode;
 import in.tf.nira.manual.verification.constant.StageCode;
@@ -228,6 +230,9 @@ public class ApplicationServiceImpl implements ApplicationService {
 	@Autowired
 	private DistrictOfficeRepository districtOfficeRepository;
 	
+	@Autowired
+	private ServiceProperties serviceProperties;
+	
 	@PostConstruct
     public void runAtStartup() {
         fetchUsers();
@@ -255,7 +260,8 @@ public class ApplicationServiceImpl implements ApplicationService {
 			logger.info("Assigning application to officer: " + selectedOfficer.getUserId());
 			MVSApplication mVSApplication = new MVSApplication();
 			mVSApplication.setRegId(verifyRequest.getRegId());
-			mVSApplication.setService(env.getProperty(verifyRequest.getService().replaceAll(" ", "_")));
+			//mVSApplication.setService(env.getProperty(verifyRequest.getService().replaceAll(" ", "_")));
+			mVSApplication.setService(serviceProperties.toDisplay(verifyRequest.getService()));
 			mVSApplication.setServiceType(env.getProperty(verifyRequest.getServiceType().replaceAll(" ", "_")));
 			mVSApplication.setReferenceURL(verifyRequest.getReferenceURL());
 			mVSApplication.setSource(verifyRequest.getSource() != null ? verifyRequest.getSource() : defaultSource);
@@ -1166,6 +1172,7 @@ public class ApplicationServiceImpl implements ApplicationService {
 			response.setRegId(application.getRegId());
 			response.setStatus(StageCode.APPROVED.getStage());
 			response.setComment(comment);
+			response.setService(serviceProperties.toCode(application.getService()));
 			ResponseEntity<Object> responseEntity = new ResponseEntity<>(response, HttpStatus.OK);
 			listener.sendToQueue(responseEntity, 1);
 		} catch (JsonProcessingException | UnsupportedEncodingException e) {
@@ -1191,6 +1198,7 @@ public class ApplicationServiceImpl implements ApplicationService {
 			response.setStatus(StageCode.REJECTED.getStage());
 			response.setComment(comment);
 			response.setCategory(rejectionCategory);
+			response.setService(serviceProperties.toCode(application.getService()));
 			response.setActionDate(LocalDate.now().format(formatter));
 			ResponseEntity<Object> responseEntity = new ResponseEntity<>(response, HttpStatus.OK);
 			listener.sendToQueue(responseEntity, 1);
