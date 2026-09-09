507f58620631d2833c1752e0be4975913f4f7249
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2026-09-09 16:00:25 +0530

    Added authorization check to take action on any application.

--- src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
+++ src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
@@ -642,6 +642,15 @@ public class ApplicationServiceImpl implements ApplicationService {
 		
 		MVSApplication application = getApplicationById(applicationId);
 		
+		String loggedInUserId = UserDetailUtil.getLoggedInUserId();		
+		
+		if(loggedInUserId == null || !loggedInUserId.equals(application.getAssignedOfficerId())) {
+			logger.error("User {} is not authorized to act on application Id {})",
+					loggedInUserId, applicationId);
+			throw new RequestException(ErrorCode.USER_NOT_AUTHORIZED.getErrorCode(),
+					ErrorCode.USER_NOT_AUTHORIZED.getErrorMessage());
+		}
+		
 		switch (request.getStatus()) {
 			case CommonConstants.APPROVE_STATUS:
 				approveApplication(application, request.getComment());
@@ -819,6 +828,15 @@ public class ApplicationServiceImpl implements ApplicationService {
 		logger.info("Scheduling interview for ID: {}", applicationId);
 		
 		MVSApplication application = getApplicationById(applicationId);
+		
+		String loggedInUserId = UserDetailUtil.getLoggedInUserId();		
+		
+		if(loggedInUserId == null || !loggedInUserId.equals(application.getAssignedOfficerId())) {
+			logger.error("User {} is not authorized to act on application Id {})",
+					loggedInUserId, applicationId);
+			throw new RequestException(ErrorCode.USER_NOT_AUTHORIZED.getErrorCode(),
+					ErrorCode.USER_NOT_AUTHORIZED.getErrorMessage());
+		}
 		
 		if(application.getAssignedOfficerRole().equals(CommonConstants.MVS_DISTRICT_OFFICER_ROLE) ||
 				application.getAssignedOfficerRole().equals(CommonConstants.MVS_LEGAL_OFFICER_ROLE) ||
