8aeed6cc576d5285ee03c93bc20dbb052cde0814
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2026-09-09 16:47:58 +0530

    Added logger

--- src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
+++ src/main/java/in/tf/nira/manual/verification/service/impl/ApplicationServiceImpl.java
@@ -635,6 +635,9 @@ public class ApplicationServiceImpl implements ApplicationService {
         
         String loggedInUserId = UserDetailUtil.getLoggedInUserId();		
 		
+        logger.info("DEBUG - loggedInUserId='{}', assignedOfficerId='{}'",
+                loggedInUserId, application.getAssignedOfficerId());
+        
 		if(loggedInUserId == null || !loggedInUserId.equals(application.getAssignedOfficerId())) {
 			logger.error("User {} is not authorized to act on application Id {})",
 					loggedInUserId, applicationId);
@@ -652,6 +655,9 @@ public class ApplicationServiceImpl implements ApplicationService {
 		MVSApplication application = getApplicationById(applicationId);
 		
 		String loggedInUserId = UserDetailUtil.getLoggedInUserId();		
+		
+		logger.info("DEBUG - loggedInUserId='{}', assignedOfficerId='{}'",
+		        loggedInUserId, application.getAssignedOfficerId());
 		
 		if(loggedInUserId == null || !loggedInUserId.equals(application.getAssignedOfficerId())) {
 			logger.error("User {} is not authorized to act on application Id {})",
